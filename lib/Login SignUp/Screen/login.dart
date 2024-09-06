8317b42b5181dd3fff79f7f7d4c58ce0e662e4f8
Sachin Prabuditha <sachinprabuditha@gmail.com>
2024-09-06 22:39:52 +0530

    Add Firebase plugins and dependencies, Create Login and sign up basic features with firebase database

---
+++ lib/Login SignUp/Screen/login.dart
@@ -0,0 +1,189 @@
+import 'package:flutter/material.dart';
+import 'package:visioncart/Login%20SignUp/Widget/button.dart';
+import 'package:visioncart/Login%20With%20Google/google_auth.dart';
+import 'package:visioncart/Password%20Forgot/forgot_password.dart';
+import 'package:visioncart/Phone%20Auth/phone_login.dart';
+import '../Services/authentication.dart';
+import '../Widget/snackbar.dart';
+import '../Widget/text_field.dart';
+import 'home_screen.dart';
+import 'signup.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _SignupScreenState();
+}
+
+class _SignupScreenState extends State<LoginScreen> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  bool isLoading = false;
+
+  @override
+  void dispose() {
+    super.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+  }
+
+// email and passowrd auth part
+  void loginUser() async {
+    setState(() {
+      isLoading = true;
+    });
+    // signup user using our authmethod
+    String res = await AuthMethod().loginUser(
+        email: emailController.text, password: passwordController.text);
+
+    if (res == "success") {
+      setState(() {
+        isLoading = false;
+      });
+      //navigate to the home screen
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          builder: (context) => const HomeScreen(),
+        ),
+      );
+    } else {
+      setState(() {
+        isLoading = false;
+      });
+      // show error
+      showSnackBar(context, res);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.white,
+      body: SafeArea(
+          child: SizedBox(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: height / 2.7,
+              child: Image.asset('assets/images/login.jpg'),
+            ),
+            TextFieldInput(
+                icon: Icons.person,
+                textEditingController: emailController,
+                hintText: 'Enter your email',
+                textInputType: TextInputType.text),
+            TextFieldInput(
+              icon: Icons.lock,
+              textEditingController: passwordController,
+              hintText: 'Enter your passord',
+              textInputType: TextInputType.text,
+              isPass: true,
+            ),
+            //  we call our forgot password below the login in button
+            const ForgotPassword(),
+            MyButtons(onTap: loginUser, text: "Log In"),
+
+            Row(
+              children: [
+                Expanded(
+                  child: Container(height: 1, color: Colors.black26),
+                ),
+                const Text("  or  "),
+                Expanded(
+                  child: Container(height: 1, color: Colors.black26),
+                )
+              ],
+            ),
+            // for google login
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+              child: ElevatedButton(
+                style:
+                    ElevatedButton.styleFrom(backgroundColor: Colors.blueGrey),
+                onPressed: () async {
+                  await FirebaseServices().signInWithGoogle();
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const HomeScreen(),
+                    ),
+                  );
+                },
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8),
+                      child: Image.network(
+                        "https://ouch-cdn2.icons8.com/VGHyfDgzIiyEwg3RIll1nYupfj653vnEPRLr0AeoJ8g/rs:fit:456:456/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9wbmcvODg2/LzRjNzU2YThjLTQx/MjgtNGZlZS04MDNl/LTAwMTM0YzEwOTMy/Ny5wbmc.png",
+                        height: 35,
+                      ),
+                    ),
+                    const SizedBox(width: 10),
+                    const Text(
+                      "Continue with Google",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+           // for phone authentication 
+           const PhoneAuthentication(),
+            // Don't have an account? got to signup screen
+            Padding(
+              padding: const EdgeInsets.only(top: 10, left: 100),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text("Don't have an account? "),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => const SignupScreen(),
+                        ),
+                      );
+                    },
+                    child: const Text(
+                      "SignUp",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      )),
+    );
+  }
+
+  Container socialIcon(image) {
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 32,
+        vertical: 15,
+      ),
+      decoration: BoxDecoration(
+        color: const Color(0xFFedf0f8),
+        borderRadius: BorderRadius.circular(12),
+        border: Border.all(
+          color: Colors.black45,
+          width: 2,
+        ),
+      ),
+      child: Image.network(
+        image,
+        height: 40,
+      ),
+    );
+  }
+}
---
+++ lib/Login SignUp/Services/authentication.dart
@@ -0,0 +1,68 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthMethod {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // SignUp User
+
+  Future<String> signupUser({
+    required String email,
+    required String password,
+    required String name,
+  }) async {
+    String res = "Some error Occurred";
+    try {
+      if (email.isNotEmpty ||
+          password.isNotEmpty ||
+          name.isNotEmpty) {
+        // register user in auth with email and password
+        UserCredential cred = await _auth.createUserWithEmailAndPassword(
+          email: email,
+          password: password,
+        );
+        // add user to your  firestore database
+        print(cred.user!.uid);
+        await _firestore.collection("users").doc(cred.user!.uid).set({
+          'name': name,
+          'uid': cred.user!.uid,
+          'email': email,
+        });
+
+        res = "success";
+      }
+    } catch (err) {
+      return err.toString();
+    }
+    return res;
+  }
+
+  // logIn user
+  Future<String> loginUser({
+    required String email,
+    required String password,
+  }) async {
+    String res = "Some error Occurred";
+    try {
+      if (email.isNotEmpty || password.isNotEmpty) {
+        // logging in user with email and password
+        await _auth.signInWithEmailAndPassword(
+          email: email,
+          password: password,
+        );
+        res = "success";
+      } else {
+        res = "Please enter all the fields";
+      }
+    } catch (err) {
+      return err.toString();
+    }
+    return res;
+  }
+
+  // for sighout
+  signOut() async {
+    // await _auth.signOut();
+  }
+}
