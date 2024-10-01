ea7a30a4e445bec5ee26cdc0e67ffdb024bed003
SanjukaLiyanage <liyanagesanju36@gmail.com>
2024-10-01 22:26:10 +0530

    change ui

--- lib/items/screens/item_list_page.dart
+++ lib/items/screens/item_list_page.dart
@@ -51,7 +51,10 @@ class _ItemListPageState extends State<ItemListPage> {
       results = allItems;
     } else {
       results = allItems.where((item) {
-        var itemName = (item.data() as Map<String, dynamic>)['name']?.toString().toLowerCase() ?? '';
+        var itemName = (item.data() as Map<String, dynamic>)['name']
+                ?.toString()
+                .toLowerCase() ??
+            '';
         return itemName.contains(query.toLowerCase());
       }).toList();
     }
@@ -110,70 +113,102 @@ class _ItemListPageState extends State<ItemListPage> {
             ? const Center(
                 child: Text('No items found', style: TextStyle(fontSize: 20)),
               )
-            : ListView.builder(
+            : GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2, // Two columns
+                  mainAxisSpacing: 10.0,
+                  crossAxisSpacing: 10.0,
+                  childAspectRatio: 0.65, // Adjust for card aspect ratio
+                ),
                 itemCount: filteredItems.length,
+                padding: const EdgeInsets.all(10),
                 itemBuilder: (context, index) {
-                  var itemData = filteredItems[index].data() as Map<String, dynamic>?; // Use nullable type
+                  var itemData =
+                      filteredItems[index].data() as Map<String, dynamic>?;
                   String id = filteredItems[index].id;
-                  String name = itemData?['name'] ?? 'Unnamed Item'; // Null check
-                  String description = itemData?['description'] ?? 'No description'; // Null check
-                  double price = itemData?['price']?.toDouble() ?? 0.0; // Null check
-                  double quantity = itemData?['quantity']?.toDouble() ?? 0.0; // Null check
-                  String imageUrl = itemData?['imageUrl'] ?? ''; // Null check
+                  String name = itemData?['name'] ?? 'Unnamed Item';
+                  String description =
+                      itemData?['description'] ?? 'No description';
+                  double price = itemData?['price']?.toDouble() ?? 0.0;
+                  double quantity = itemData?['quantity']?.toDouble() ?? 0.0;
+                  String imageUrl = itemData?['imageUrl'] ?? '';
 
-                  return Card(
-                    color: Colors.white,
-                    elevation: 3,
-                    margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 8),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 8.0),
-                      child: ListTile(
-                        contentPadding: const EdgeInsets.all(8.0),
-                        leading: imageUrl.isNotEmpty
-                            ? ClipRRect(
-                                borderRadius: BorderRadius.circular(8.0),
-                                child: Image.network(imageUrl,
-                                    width: 80, height: 80, fit: BoxFit.cover),
-                              )
-                            : const Icon(Icons.image, size: 50),
-                        title: Text(
-                          name,
-                          style: const TextStyle(
-                              fontWeight: FontWeight.bold, color: Colors.black),
-                        ),
-                        subtitle: Text(
-                          'Total Sales(Qty) - ${quantity.toInt()}',
-                          style: const TextStyle(color: Colors.blueGrey, fontSize: 16),
-                        ),
-                        trailing: Text(
-                          '\$${price.toStringAsFixed(2)}',
-                          style: const TextStyle(
-                            color: Colors.green,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 16,
+                  return GestureDetector(
+                    onTap: () {
+                      // Navigate to the ItemCard page when tapped
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => ItemCard(
+                            id: id,
+                            name: name,
+                            description: description,
+                            price: price,
+                            initialQuantity: quantity,
+                            imageUrl: imageUrl,
+                            onQuantityChanged: (newQuantity) {
+                              _updateItemQuantity(id, newQuantity);
+                            },
                           ),
                         ),
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => ItemCard(
-                                id: id,
-                                name: name,
-                                description: description,
-                                price: price,
-                                initialQuantity: quantity,
-                                imageUrl: imageUrl,
-                                onQuantityChanged: (newQuantity) {
-                                  _updateItemQuantity(id, newQuantity);
-                                },
+                      );
+                    },
+                    child: Card(
+                      color: Colors.white,
+                      elevation: 3,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Expanded(
+                            child: ClipRRect(
+                              borderRadius: const BorderRadius.only(
+                                topLeft: Radius.circular(10),
+                                topRight: Radius.circular(10),
                               ),
+                              child: imageUrl.isNotEmpty
+                                  ? Image.network(imageUrl,
+                                      width: double.infinity,
+                                      fit: BoxFit.cover)
+                                  : const Icon(Icons.image,
+                                      size: 80), // Placeholder
                             ),
-                          );
-                        },
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  name,
+                                  style: const TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 16,
+                                  ),
+                                ),
+                                const SizedBox(height: 5),
+                                Text(
+                                  'Total Sales: ${quantity.toInt()}',
+                                  style: const TextStyle(
+                                    color: Colors.blueGrey,
+                                    fontSize: 14,
+                                  ),
+                                ),
+                                const SizedBox(height: 5),
+                                Text(
+                                  '\$${price.toStringAsFixed(2)}',
+                                  style: const TextStyle(
+                                    color: Colors.green,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 14,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
                       ),
                     ),
                   );
